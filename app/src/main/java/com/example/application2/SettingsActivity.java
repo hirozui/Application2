17646a7500b4e29b4b1367e99a4cbff7cee5773e
hirozui <18171901585@163.com>
2020-10-27 21:48:00 +0800

    配置页面

---
+++ app/src/main/java/com/example/application2/SettingsActivity.java
@@ -0,0 +1,51 @@
+package com.example.application2;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+
+            Preference setText1 = findPreference(getString(R.string.set_text1_key));
+            bindPreferenceSummaryToValue(setText1);
+
+            //设置选项2可显示
+            //bindPreferenceSummaryToValue(findPreference(getString(R.string.set_text2_key)));
+
+            //设置显示列表值，会替换掉原来的summary显示
+            //bindPreferenceSummaryToValue(findPreference(getString(R.string.set_list_key)));
+        }
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+    }
+}
